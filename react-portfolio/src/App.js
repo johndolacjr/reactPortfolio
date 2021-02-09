772d722ab30415dc808eefd5e4cede17bd1fce2c
johndolac <johndolacjr@gmail.com>
2021-02-09 13:24:41 -0700

    created react app and  navbar link

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from './components/Navbar';
+// import Navbar from '../components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <NavBar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+    </Router>
+    </>
+  );
+}
+
+export default App;
