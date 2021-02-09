ecb0e7e682d53a94565ff88bf8d01f5bea968474
johndolac <johndolacjr@gmail.com>
2021-02-09 14:16:36 -0700

    created button files

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -5,6 +5,7 @@ function NavBar() {
     const [click, setClick] = useState(false);
     
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
 
     return (
         <>
@@ -16,6 +17,29 @@ function NavBar() {
                   <div className='.menu-icon' onClick={handleClick}>
                       <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                   </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/aboutMe' className='nav-links' onClick={closeMobileMenu}>
+                                About Me
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/portfolio' className='nav-links' onClick={closeMobileMenu}>
+                                Portfolio
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/photos' className='nav-links' onClick={closeMobileMenu}>
+                                Photos
+                            </Link>
+                        </li>
+                    </ul>
+                    
               </div>
           </nav>
         </>
